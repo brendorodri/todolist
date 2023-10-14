e7c60606ee34697e500c77bec765694101be3331
brendorodri <brendorodriguesdebrito@gmail.com>
2023-10-14 15:38:27 -0300

    Docker file deploy

---
+++ src/test/java/br/com/brendorodri/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brendorodri.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
